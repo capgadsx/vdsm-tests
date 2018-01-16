5de75e912ac707ea9c969263169ebe665f271d8c
Carlos Andres Ponce Godoy <cponce@alumnos.inf.utfsm.cl>
2018-01-16 16:51:40 -0300

    Added test run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+set -xe
+
+docker run --privileged -d \
+---net=host \
+-device /dev/kvm:/dev/kvm \
+-v /var/lib/libvirt:/var/lib/libvirt:rw \
+-v /var/run/libvirt:/var/run/libvirt:rw \
+-v /sys/fs/cgroup:/sys/fs/cgroup:rw \
+labcompovirt/libvirtd
