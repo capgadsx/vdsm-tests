8d9261d80c43f7661be9599c616b1b9bf118bffc
Carlos Andres Ponce Godoy <cponce@alumnos.inf.utfsm.cl>
2018-01-17 19:31:48 -0300

    Move imageio rpms and build vdsm, but at this point tests fails in the container

--- builder/27/buildovirt.sh
+++ builder/27/buildovirt.sh
@@ -8,3 +8,18 @@ patch -p0 < /root/suffixpatch.patch
 make
 make check
 make rpm
+for dir in common daemon proxy; do \
+    cp $dir/dist/*.rpm /results/; \
+done
+
+dnf install -y /results/ovirt-imageio-common*.noarch.rpm    # For vdsm
+cd /root/
+
+git clone http://gerrit.ovirt.org/p/vdsm.git
+cd vdsm
+dnf install -y `cat automation/check-patch.packages.fc27`
+./autogen.sh --system --enable-hooks
+./configure
+make
+make check
+make rpm
